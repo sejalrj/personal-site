c40de15eefdbfda8a128dd1d9aff6544390baad9
sejalrj <sejalrjagtap@gmail.com>
2022-12-01 16:58:18 -0500

    Update degrees.js

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,12 +1,12 @@
 const degrees = [
   {
-    school: 'New York University, NY, USA',
+    school: 'New York University',
     degree: 'M.S. Computer Science',
     link: 'https://nyu.edu',
-    year: 2022,
+    year: 2024,
   },
   {
-    school: 'Pune Institute of Computer Technology, Pune, India',
+    school: 'Pune Institute of Computer Technology',
     degree: 'B.E. Information Technology',
     link: 'https://pict.edu',
     year: 2017,
